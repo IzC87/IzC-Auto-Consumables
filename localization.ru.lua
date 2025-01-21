10dc74f10bf508ead549288a5155fcddce189a61
Fredrik Odén <fredrikoden87@gmail.com>
2025-01-21 14:43:45 +0100

    Added russian localization. Thanks ZamestoTV/Hubbotu

---
+++ localization.ru.lua
@@ -0,0 +1,17 @@
+local _, L = ...;
+if GetLocale() == "ruRU" then
+    L["Healthstone"] = "Камень здоровья";
+    L['Requires Level'] = "Требуется уровень";
+    L["Must remain seated"] = "Необходимо оставаться на месте";
+    L["become well fed and gain"] = "стать сытым и набраться сил";
+    L["Stamina and Spirit for"] = "Выносливость и Дух для";
+    L['Use: Restores %d+ mana over'] = "Использование: Восстанавливает %d+ маны за";
+    L['Use: Restores %d+ to %d+ health'] = "Использование: Восстанавливает здоровье от %d+ до %d+";
+    L['Use: Restores %d+ to %d+ mana'] = "Использование: Восстанавливает %d+ до %d+ маны";
+    L['Use: Heals %d+ damage over'] = "Использование: Лечит %d+ урона за";
+    L['Conjured'] = "Сотворенный";
+    L["Consumable"] = "Расходный материал";
+    L["Raw Black Truffle"] = "Свежий черный трюфель";
+    L["Raw"] = "Сырой";
+    L["Requires First Aid"] = "Требуется первая помощь";
+end
